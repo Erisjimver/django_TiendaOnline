470cf4b52b42ad1a4a25206d7c8be4b872c0f737
erisjimver <erisjinver@gmail.com>
2019-11-19 09:55:03 -0500

    creacion de bd migration

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'articulosclientes',
+        'USER': 'postgres',
+        'PASSWORD':'59291',
+        'HOST':'127.0.0.1',
+        'DATABASE_PORT':'5432',
     }
 }
+
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, #'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
