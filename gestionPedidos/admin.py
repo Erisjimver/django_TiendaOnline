583e751ac690aafd829090f34066b69295b9c3a8
Erisjimver <erisjinver@gmail.com>
2020-02-21 18:09:52 -0500

    filtrar menus en django

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from gestionPedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre","direccion","telefono")
+    search_fields=("nombre","telefono")
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=("seccion",)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos,PedidosAdmin)
